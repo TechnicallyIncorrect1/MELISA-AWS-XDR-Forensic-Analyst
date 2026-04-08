553f814b6b7823fb55115041e53e246cc0aaef40
Frank Gonzalez <frank2025da@gmail.com>
2026-04-08 07:13:46 -0400

    Initialize 1139-msaad54v1-setting-discontinued-by-microsoft

---
+++ PowerShell/ScubaGear/Testing/Unit/PowerShell/Orchestrator/Invoke-ScubaCached.Tests.ps1
@@ -0,0 +1,126 @@
+$OrchestratorPath = '../../../../Modules/Orchestrator.psm1'
+Import-Module (Join-Path -Path $PSScriptRoot -ChildPath $OrchestratorPath) -Function 'Invoke-SCuBACached' -Force
+
+InModuleScope Orchestrator {
+    Describe -Tag 'Orchestrator' -Name 'Invoke-SCuBACached' {
+        BeforeAll {
+            Mock -ModuleName Orchestrator Remove-Resources {}
+            Mock -ModuleName Orchestrator Import-Resources {}
+            function Invoke-Connection {}
+            Mock -ModuleName Orchestrator Invoke-Connection { @() }
+            function Get-TenantDetail {}
+            Mock -ModuleName Orchestrator Get-TenantDetail { '{"DisplayName": "displayName"}' }
+            function Invoke-ProviderList {}
+            Mock -ModuleName Orchestrator Invoke-ProviderList {}
+            function Invoke-RunRego {}
+            Mock -ModuleName Orchestrator Invoke-RunRego {}
+            function Invoke-ReportCreation {}
+            Mock -ModuleName Orchestrator Invoke-ReportCreation {}
+            function Merge-JsonOutput {throw 'this will be mocked'}
+            Mock -ModuleName Orchestrator Merge-JsonOutput {}
+            function Disconnect-SCuBATenant {}
+            Mock -ModuleName Orchestrator Disconnect-SCuBATenant
+
+            Mock -CommandName New-Item {}
+            Mock -CommandName Get-Content {}
+        }
+        Context 'When checking the conformance of commercial tenants' {
+            BeforeAll {
+                [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'SplatParams')]
+                $SplatParams = @{
+                    M365Environment = 'commercial'
+                }
+            }
+            It 'Given -ProductNames aad should not throw' {
+                $SplatParams += @{
+                    ProductNames = @("aad")
+                }
+                {Invoke-SCuBACached @SplatParams} | Should -Not -Throw
+            }
+            It 'Given -ProductNames defender should not throw' {
+                $SplatParams += @{
+                    ProductNames = @("defender")
+                }
+                {Invoke-SCuBACached @SplatParams} | Should -Not -Throw
+            }
+            It 'Given -ProductNames exo should not throw' {
+                $SplatParams += @{
+                    ProductNames = @("exo")
+                }
+                {Invoke-SCuBACached @SplatParams} | Should -Not -Throw
+            }
+            It 'Given -ProductNames powerplatform should not throw' {
+                $SplatParams += @{
+                    ProductNames = @("powerplatform")
+                }
+                {Invoke-SCuBACached @SplatParams} | Should -Not -Throw
+            }
+            It 'Given -ProductNames teams should not throw' {
+                $SplatParams += @{
+                    ProductNames = @("teams")
+                }
+                {Invoke-SCuBACached @SplatParams} | Should -Not -Throw
+            }
+            It 'Given -ProductNames * should not throw' {
+                $SplatParams += @{
+                    ProductNames = @("*")
+                }
+                {Invoke-SCuBACached @SplatParams} | Should -Not -Throw
+            }
+        }
+        Context 'When omitting the export of the commercial tenant provider json' {
+            BeforeAll {
+                [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'SplatParams')]
+                $SplatParams = @{
+                    M365Environment = 'commercial'
+                    ExportProvider = $false
+                }
+            }
+            It 'Given -ProductNames aad should not throw' {
+                $SplatParams += @{
+                    ProductNames = @("aad")
+                }
+                {Invoke-SCuBACached @SplatParams} | Should -Not -Throw
+            }
+            It 'Given -ProductNames defender should not throw' {
+                $SplatParams += @{
+                    ProductNames = @("defender")
+                }
+                {Invoke-SCuBACached @SplatParams} | Should -Not -Throw
+            }
+            It 'Given -ProductNames exo should not throw' {
+                $SplatParams += @{
+                    ProductNames = @("exo")
+                }
+                {Invoke-SCuBACached @SplatParams} | Should -Not -Throw
+            }
+            It 'Given -ProductNames powerplatform should not throw' {
+                $SplatParams += @{
+                    ProductNames = @("powerplatform")
+                }
+                {Invoke-SCuBACached @SplatParams} | Should -Not -Throw
+            }
+            It 'Given -ProductNames teams should not throw' {
+                $SplatParams += @{
+                    ProductNames = @("teams")
+                }
+                {Invoke-SCuBACached @SplatParams} | Should -Not -Throw
+            }
+            It 'Given -ProductNames * should not throw' {
+                $SplatParams += @{
+                    ProductNames = @("*")
+                }
+                {Invoke-SCuBACached @SplatParams} | Should -Not -Throw
+            }
+        }
+        Context 'When checking module version' {
+            It 'Given -Version should not throw' {
+                {Invoke-SCuBACached -Version} | Should -Not -Throw
+            }
+        }
+    }
+}
+
+AfterAll {
+    Remove-Module Orchestrator -ErrorAction SilentlyContinue
+}
